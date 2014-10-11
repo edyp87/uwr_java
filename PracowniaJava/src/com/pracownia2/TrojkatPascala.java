35a0d3906ce6ace8184e585d03a7df43bdcc9610
Marek <edyp87@gmail.com>
2014-10-11 16:17:33 +0200

    Pracownia2. Finished implementation.

--- PracowniaJava/src/com/pracownia2/TrojkatPascala.java
+++ PracowniaJava/src/com/pracownia2/TrojkatPascala.java
@@ -1,79 +1,81 @@
 package com.pracownia2;
 
 public class TrojkatPascala {
-	public static long czytaj(int p_wiersz, int p_kolumna) throws IllegalArgumentException
+	public static long czytaj(int p_numerWiersza, int p_numerKolumny) throws IllegalArgumentException
 	{
-		walidujArgumenty(p_wiersz, p_kolumna);
-		return czyPodstawaRekursji(p_wiersz, p_kolumna) ? 
-				1 : zwrocPascala(p_wiersz, p_kolumna);
+		walidujArgumenty(p_numerWiersza, p_numerKolumny);
+		return czyPodstawaRekursji(p_numerWiersza, p_numerKolumny) ? 
+				1 : zwrocPascala(p_numerWiersza, p_numerKolumny);
 	}
 	
-	private static void walidujArgumenty(int p_wiersz, int p_kolumna)  throws IllegalArgumentException
+	private static void walidujArgumenty(int p_numerWiersza, int p_numerKolumny)  throws IllegalArgumentException
 	{
-		if (kolumnaJestWiekszaOdWiersza(p_wiersz, p_kolumna) || wartosciWychodzaPozaZakres(p_wiersz, p_kolumna))
+		if (kolumnaJestWiekszaOdWiersza(p_numerWiersza, p_numerKolumny) || wartosciWychodzaPozaZakres(p_numerWiersza, p_numerKolumny))
 		{
 			throw new IllegalArgumentException("n powinno być większe od k!");
 		}
 	}
 
-	private static long zwrocPascala(int p_wiersz, int p_kolumna)
+	private static long zwrocPascala(int p_numerWiersza, int p_numerKolumny)
 	{
-		return tablica[p_wiersz][p_kolumna];
+		return tablica[p_numerWiersza][p_numerKolumny];
 	}
 	
 	private static void wypelnijTrojkatPascala()
 	{
-		for (int l_wiersz = 0; l_wiersz < c_max; ++l_wiersz)
+		for (int l_numerWiersza = 0; l_numerWiersza < c_max; ++l_numerWiersza)
 		{
-			stworzNowyWiersz(l_wiersz);
-			wypelnijNowyWiersz(l_wiersz);
+		    stworzNowyWiersz(l_numerWiersza);
+			wypelnijNowyWiersz(l_numerWiersza);
 		}
 	}
 
-	private static void wypelnijNowyWiersz(int l_wiersz) 
+	private static void wypelnijNowyWiersz(int p_numerWiersza) 
 	{
-		for (int l_kolumna = 0; l_kolumna < l_wiersz+1; ++l_kolumna)
+		for (int p_numerKolumny = 0; p_numerKolumny < p_numerWiersza+1; ++p_numerKolumny)
 		{
-			wpiszNowaWartoscWKolumne(l_wiersz, l_kolumna);
+			wpiszNowaWartoscWKolumne(p_numerWiersza, p_numerKolumny);
 		}
 	}
 
-	private static void wpiszNowaWartoscWKolumne(int l_wiersz, int l_kolumna) 
+	private static void wpiszNowaWartoscWKolumne(int p_numerWiersza, int p_numerKolumny) 
 	{
-		if (czyPodstawaRekursji(l_wiersz, l_kolumna))
+		if (czyPodstawaRekursji(p_numerWiersza, p_numerKolumny))
 		{
-			tablica[l_wiersz][l_kolumna] = 1;
+			tablica[p_numerWiersza][p_numerKolumny] = 1;
 		}
 		else
 		{
-			tablica[l_wiersz][l_kolumna] = tablica[l_wiersz-1][l_kolumna-1] + tablica[l_wiersz-1][l_kolumna];
+			tablica[p_numerWiersza][p_numerKolumny] = 
+					tablica[p_numerWiersza-1][p_numerKolumny-1] +
+					tablica[p_numerWiersza-1][p_numerKolumny  ];
 		}
 	}
 	
-	private static void stworzNowyWiersz(int p_wiersz)
+	private static void stworzNowyWiersz(int p_numerWiersza)
 	{
-		tablica[p_wiersz] = new long[p_wiersz+1];
+		tablica[p_numerWiersza] = new long[p_numerWiersza+1];
 	}
 	
-	private static Boolean czyPodstawaRekursji(int p_wiersz, int p_kolumna)
+	private static Boolean czyPodstawaRekursji(int p_numerWiersza, int p_numerKolumny)
 	{
-		return p_kolumna == 0 || p_kolumna == p_wiersz;
+		return p_numerKolumny == 0 || p_numerKolumny == p_numerWiersza;
 	}
 	
-	private static boolean kolumnaJestWiekszaOdWiersza(int p_wiersz, int p_kolumna) 
+	private static boolean kolumnaJestWiekszaOdWiersza(int p_numerWiersza, int p_numerKolumny) 
 	{
-		return p_kolumna > p_wiersz;
+		return p_numerKolumny > p_numerWiersza;
 	}
 	
-	private static boolean wartosciWychodzaPozaZakres(int p_wiersz, int p_kolumna)
+	private static boolean wartosciWychodzaPozaZakres(int p_numerWiersza, int p_numerKolumny)
 	{
-		return     p_kolumna >= c_max 
-				|| p_wiersz  >= c_max 
-				|| p_kolumna < 0 
-				|| p_wiersz  < 0;
+		return     p_numerKolumny >= c_max 
+				|| p_numerWiersza  >= c_max 
+				|| p_numerKolumny < 0 
+				|| p_numerWiersza  < 0;
 	}
 	
-	private final static int c_max   = 31;
+	public final static int c_max   = 31;
 	private static long[][]  tablica = new long[c_max][];
 	
 	static {
