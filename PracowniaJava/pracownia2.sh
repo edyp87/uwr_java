aa9fd4d23ca0a457ce31cca87c65287b48ae62cd
Marek <edyp87@gmail.com>
2014-10-15 21:55:46 +0200

    Pracownia2. Done some refactor and add running script.

---
+++ PracowniaJava/pracownia2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Compile
+javac \
+    src/com/pracownia2/WzorDwumianowy.java \
+    src/com/pracownia2/TrojkatPascala.java \
+    src/com/pracownia2/Pracownia2.java
+
+array=(
+    a+b   1
+    a-b   1
+    a+b   0
+    s+t   5
+    s-t   5
+    s-t   66
+
+    a+b   67
+    a*b   1
+    a/b   1
+    +b    1
+    -a+b  1
+    a+b  -1
+)
+
+#Run
+for (( i = 0 ; i < ${#array[@]} ; i++ )) do
+
+    if (( i % 2 != 0 ))    
+    then
+        continue
+    fi
+    
+    java -classpath src com/pracownia2/Pracownia2 ${array[${i}]} ${array[${i}+1]}
+    echo "" 
+
+done
+
